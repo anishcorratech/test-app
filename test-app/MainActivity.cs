f0a18ad43388633d8871b8b88f7d10687357cccb
anishcorratech <anish.asokan@corra.com>
2013-12-15 04:30:45 +0530

    * MainActivity.cs: Adding hello world to app
    * AssemblyInfo.cs:
    * R.java:

--- test-app/MainActivity.cs
+++ test-app/MainActivity.cs
@@ -17,16 +17,21 @@ namespace testapp
 		{
 			base.OnCreate (bundle);
 
-			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
+			//Create the user interface in code
+			var layout = new LinearLayout (this);
+			layout.Orientation = Orientation.Vertical;
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
-			};
+			var aLabel = new TextView (this);
+			aLabel.Text = "Hello, Xamarin.Android";
+
+			var aButton = new Button (this);      
+			aButton.Text = "Say Hello";
+			aButton.Click += (sender, e) => {
+				aLabel.Text = "Hello from the button";
+			};  
+			layout.AddView (aLabel);
+			layout.AddView (aButton);           
+			SetContentView (layout);
 		}
 	}
 }
